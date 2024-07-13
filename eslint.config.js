952f3f45f86dbf647c502a0560fb47b95bbaabe0
Hugues Blanco <contact@huguesblanco.com>
2024-07-13 11:27:55 +0200

    Install ESLint
    - Installed ESLint for the TypeScript project using `typescript-eslint`.
    - Added a configuration file for ESLint with a "flat config" structure.
    - Integrated `strictTypeChecked` and `stylisticTypeChecked` shareable configurations.
    - Added a linter script to `package.json`.
    
    Note: `eslint-plugin-react-hooks` and `eslint-plugin-react-refresh` have not been reinstalled yet.

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  }
+);
